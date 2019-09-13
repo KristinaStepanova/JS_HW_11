e547b6b84ede37a8d53b6ce8bfd9c7f5326406a1
KristinaStepanova <kri18stina@gmail.com>
2019-09-13 18:10:16 +0300

    task 2

--- app.js
+++ app.js
@@ -10,29 +10,51 @@
 //     } catch(err) {
 //         alert(err);
 //     }
-    
+
 // }
 
 // getKitty();
 
 //task 2
 
-const getUsers = async() => {
-    try{
-       const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-       const result = await response.json();
-       const ul = document.createElement('ul');
-       document.body.appendChild(ul);
-       result.forEach(el => {
-           const li = document.createElement('li');
-           li.innerHTML = `Пользователь userID=${userId} имеет завершенных и не завершенных заданий`;
-           ul.appendChild(li);
-       });
-    console.log(result);
-    } catch(err) {
-        alert(err);
+const getUsers = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const result = await response.json();
+    const ul = document.createElement("ul");
+    document.body.appendChild(ul);
+    for (let i = 1; i <= 10; i++) {
+      let user = result.filter(item => item.userId === i);
+      let completed = user.filter(item => item.completed).length;
+      const li = document.createElement("li");
+      li.innerHTML = `Пользователь userID=${i} имеет ${completed} завершенных и ${user.length -
+        completed} не завершенных заданий`;
+      ul.appendChild(li);
     }
-    
-}
+  } catch (err) {
+    alert(err);
+  }
+};
 
 getUsers();
+
+//task 3
+
+
+
+const postUsers = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify();
+    });
+    const result = await response.json();
+  } catch (err) {
+    alert(err);
+  }
+};
+
+postUsers();
